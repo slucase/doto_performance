6677997ff1e01fbc5b4f6cbce82de0b21ba2061d
Lucas Silveira <lucassilveira@dcc.ufmg.br>
2018-07-24 18:50:58 -0300

    WIP: Create DB connection (mysql)

--- main.py
+++ main.py
@@ -2,26 +2,43 @@
 import dota2api
 import json
 import my_auth
-from collections import namedtuple
+import pymysql
 
 user_id = my_auth.user_id
 api_key = my_auth.api_key
 
+# DB connection
+connection = pymysql.connect(host='localhost',
+                             user=my_auth.db_user,
+                             password=my_auth.db_pass,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
 api = dota2api.Initialise(api_key)
 history = api.get_match_history(account_id=user_id)
 
-#Loop for getting the matches ids
+# Loop for getting the matches ids
 my_matches = []
 n_results = history['num_results']
 
 for i in range(n_results):
-   my_matches.append(history['matches'][i]['match_id'])
+    my_matches.append(history['matches'][i]['match_id'])
 
-#Loop for getting the friends and nemesis data
-for i in range(n_results):
-   m = api.get_match_details(match_id = my_matches[i])
-   player[i] = m['players'][i]['player_slot']
-   print(player)
+try:
+   with connection.cursor() as cursor:
+      #Create table
+      sql = "CREATE TABLE ( player int, hero int)"
+      cursor.execute(sql)
+   connection.commit()
+finally:
+   connection.close()
 
-#print(api.get_heroes())
-#print(my_matches)
+
+# Loop for getting the friends and nemesis data
+# for i in range(n_results):
+#   m = api.get_match_details(match_id = my_matches[i])
+#   player[i] = m['players'][i]['player_slot']
+#   print(player)
+
+# print(api.get_heroes())
+# print(my_matches)
